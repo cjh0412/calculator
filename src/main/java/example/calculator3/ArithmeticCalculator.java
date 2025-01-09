8598561a2c54e659479ac0bd9ba682bb686a6999
jyedev04 <jyedev04@gmail.com>
2025-01-09 18:36:03 +0900

    계산기 lv3 수정  - 특정 값 비교 후 출력 시 정수, 실수 구분 수정  - 출력 메서드 추가

--- src/main/java/example/calculator3/ArithmeticCalculator.java
+++ src/main/java/example/calculator3/ArithmeticCalculator.java
@@ -16,7 +16,7 @@ public class ArithmeticCalculator<T extends Number> {
 
     // input 값은 숫자 타입에 관계없이 저장
     private T num1, num2;
-    List<T> list = new ArrayList<>();
+    private List<T> list = new ArrayList<>();
 
     public void setNum(T num1, T num2) {
         this.num1 = num1;
@@ -32,13 +32,15 @@ public class ArithmeticCalculator<T extends Number> {
     }
 
     // 가장 마지막에 저장된 값 출력
-    public T getLastValue() {
-        return list.get(list.size() - 1);
+    void getLastValue() {
+        System.out.print("결과 : ");
+        isInteger(list.get(list.size() - 1));
     }
 
     // 삭제
-    T delete() {
-        return list.remove(0);
+    void delete() {
+        System.out.print("삭제된 값 : ");
+        isInteger(list.remove(0));
     }
 
     // 저장
@@ -46,13 +48,27 @@ public class ArithmeticCalculator<T extends Number> {
         list.add(result);
     }
 
-    public List<T> getList(T compareValue) {
+    public void getList(T compareValue) {
         // 마지막에 입력된 값보다 큰 값 출력
 //      오류 원인 1. T는 Number를 상속 받았더라도 추상적인 형태를 지님
 //               2. 숫자타입이 아닌 T의 경우 연산자(+,-,*,/,>,>= ....)를 사용하기 위해선 기본타입 int, double 등으로 변경 필요!
 //               3. mapToDouble(Double::parseDouble) :  String -> double형태로 변환하는 것이기 때문에 오류 발생
 //        list.stream().filter(i -> i > getList()).mapToDouble(Double::parseDouble).toList();
-        return list.stream().filter(i -> i.doubleValue() > compareValue.doubleValue()).toList();
+
+// return list.stream().filter(i -> i.doubleValue() > compareValue.doubleValue()).toList();
+        printList(
+                (List<Number>) list.stream()
+                .filter(i -> i.doubleValue() > compareValue.doubleValue()) // 입력값과 비교
+                .map(i -> {
+                    if(i.doubleValue() == i.intValue()){
+                        return i.intValue();
+                    }else {
+                        return i.doubleValue() ;
+                    } // 정수 실수 구분
+                })
+                .toList()
+        );
+        
     }
 
     // 숫자여부 체크(num1, num2)
@@ -72,8 +88,11 @@ public class ArithmeticCalculator<T extends Number> {
     }
 
     // 정수 여부 체크
-    public boolean isInteger(T result) {
-        return result.doubleValue() == result.intValue();
+    public void isInteger(T result) {
+        boolean integerYn = result.doubleValue() == result.intValue();
+        
+        //결과값 출력
+        printValue(integerYn, result);
     }
 
     // 사칙 연산시 제네릭 타입의 경우 연산 불가
@@ -100,6 +119,23 @@ public class ArithmeticCalculator<T extends Number> {
     // 소수점 2자리수 이상은 반올림한다
     double roundValue(double result) {
         return (double) Math.round(result * 100) / 100;
+    }
+
+    // 정수여부 체크후 결과값 출력
+    void printValue(boolean integerYn, T result){
+        if(integerYn){
+            System.out.println(result.intValue());;
+        }else {
+            System.out.println(result.doubleValue());
+        }
+    }
+
+    // 리스트 출력
+    void printList(List<Number> resultList){
+        if(resultList.isEmpty()){
+            System.out.println("해당 연산 값 보다 더 큰 저장된 연산 결과는 존재하지 않습니다.");
+        }
+        System.out.println("해당 연산 값 보다 더 큰 저장된 연산 결과: " + resultList);
     }
 
 }
